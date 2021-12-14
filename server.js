4548b6f042d2265d37a8190e3baa7bd06c86a54e
Jathin <jathinr96@gmail.com>
2021-12-14 09:19:19 +0530

    dsfsdf

---
+++ server.js
@@ -0,0 +1,23 @@
+let express  = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.send('<h1>Welcome to Node.js project setup</h1>')
+})
+
+app.get('/test', function(req, res) {
+    console.log(req)
+	res.send('<h1>Welcome to Node.js project setup</h1>')
+})
+
+app.post('/test', function(req, res) {
+    console.log(req)
+	res.send('<h1>Welcome to Node.js project setup</h1>')
+})
+
+app.listen(3000, function(){
+	console.log("Server started on port: 3000")
+})
